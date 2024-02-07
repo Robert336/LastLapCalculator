5bf7fa7850ce72b286041cb576e9ae67f19c89a4
Robert Mazza <rmazza33@gmail.com>
2024-02-06 20:50:36 -0500

    feat: Implement base Input component and time input varient component.

---
+++ src/components/ui/NumberInput.tsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import { Input } from './Input';
+
+interface NumberInputProps {
+    value: string
+    onChange: (value: string) => void;
+}
+
+export const NumberInput: React.FC<NumberInputProps> = ({value, onChange}, ...props) => {
+
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>){
+        const value = event.target.value;
+
+        // only handle change if input is a number
+        if (value.match(/[0-9]/) || value == ''){
+            onChange(event.target.value);
+        }
+    }
+
+    return (
+        <Input
+        type="text"
+        onChange={handleChange}
+        value={value}
+        />
+    );
+}
