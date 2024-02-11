34c1d3545c83b1cde5a194ecbf24e0ddcb0dd209
Robert336 <rmazza33@gmail.com>
2024-02-11 15:14:53 -0500

    refactor/fix: remove unessesary functions, add new functions, and fix logic errors.

--- src/utils/timeUtils.ts
+++ src/utils/timeUtils.ts
@@ -1,93 +1,75 @@
 
-// output MM:SS.mmm
+// output MM:SS
 function formatTime(timeStr: string): string {
-    let formattedTime = timeStr.padStart(7, '0');
-    formattedTime = formattedTime.slice(0, 2) + ':' + formattedTime.slice(2, 4) + '.' + formattedTime.slice(4,7);
-    return formattedTime;
-  }
+  let formattedTime = timeStr.padStart(4, '0');
+  formattedTime = formattedTime.slice(0, 2) + ':' + formattedTime.slice(2, 4)
+  return formattedTime;
+}
 
+function pad(num: number): string {
+  return num < 10 ? '0' + String(num) : String(num);
+}
 
 /*
   Takes time string in format MM:SS.mmm and converts it to milliseconds as a number.
+  // TODO: Change to just MM:SS
 */
 function timeStrToNumber(str: string): number {
   
   const mins = Number(str.slice(0, 2)) * 60000;
-  const secs = Number(str.slice(3, 5)) * 1000;
-  const ms = mins + secs + Number(str.slice(6));
-
+  const secs = Number(str.slice(3, 4)) * 1000;
+  const ms = mins + secs;
   return ms;
 }
 
+
 function millisecondsToTimeStr(ms: number): string {
   
-  // 100 ms = 1 sec
+  // 1000 ms = 1 sec
   // 60 sec = 1 min
-  // 6000 ms = 1 min
-
+  // 60000 ms = 1 min
+  let carry = 0;
   const mins = Math.floor(ms / 60000);
-  const secs_remaining = Math.floor(((ms - mins * 60000) / 6000));
-  const ms_remaining = Math.floor((ms - secs_remaining * 6000 - mins * 60000));
-
-  const unformattedTimeStr = String(mins) + String(secs_remaining) + String(ms_remaining);
-
-  return formatTime(unformattedTimeStr);
+  // left over ms from calculating mins
+  carry = ms - mins * 60000
+  
+  const secs_remaining = Math.floor(carry / 1000);
+  carry = carry - secs_remaining * 1000
+  
+  return pad(mins) + ':' + pad(secs_remaining);
 }
+
 
 function addTimeDigit(timeStr: string, digit: string): string {
 
   let tempStr = timeStr.replaceAll(':', '');
-  tempStr = tempStr.replaceAll('.', '');
-  
+
   tempStr = tempStr + digit;
 
-  // need to adjust : and .
   const tempLength = tempStr.length;
-  const milisec = tempStr.slice(tempLength - 3, tempLength);
-  const sec = tempStr.slice(tempLength - 5, tempLength - 3);
-  const min = tempStr.slice(tempLength - 7, tempLength - 5);
+  // const milisec = tempStr.slice(tempLength - 3, tempLength);
+  const sec = tempStr.slice(tempLength - 2, tempLength);
+  const min = tempStr.slice(tempLength - 4, tempLength - 2);
 
-  return min + ':' + sec + '.' + milisec;
+  return min + ':' + sec;
 }
+
 
 function removeTimeDigit(timeStr: string): string {
 
   let tempStr = timeStr.replaceAll(':', '');
-  tempStr = tempStr.replaceAll('.', '');
+  //tempStr = tempStr.replaceAll('.', '');
   
-  tempStr = '0' + tempStr.substring(0,7);
+  tempStr = '0' + tempStr.substring(0,4);
 
   return formatTime(tempStr);
-}
-
-function timeStrAddition(str1: string, str2: string): string {
-  // convert both strings into milliseconds as a number
-  // compute addition in ms
-  // convert ms back to formatted time str
-  const strNum1 = timeStrToNumber(str1);
-  const strNum2 = timeStrToNumber(str2);
-
-  const sumInMS = strNum1 + strNum2;
-
-  return millisecondsToTimeStr(sumInMS);
-}
-
-function timeStrMultiply(str: string, factor: number): string {
-  // convert both strings into milliseconds as number
-  // compute multiplication in ms
-  // convert ms back to formatted time str
-
-  const strNum = timeStrToNumber(str) * factor;
-
-  return millisecondsToTimeStr(strNum);
 }
 
 
 export {
   formatTime,
   addTimeDigit,
+  millisecondsToTimeStr,
   removeTimeDigit,
-  timeStrAddition,
-  timeStrMultiply,
   timeStrToNumber,
 };
