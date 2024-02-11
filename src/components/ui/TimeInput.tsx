e9c6dfe7304052e8bc9503035eee5744f27d3f8e
Robert336 <rmazza33@gmail.com>
2024-02-11 15:15:52 -0500

    chore: remove unused lines

--- src/components/ui/TimeInput.tsx
+++ src/components/ui/TimeInput.tsx
@@ -3,40 +3,34 @@ import {Input, InputProps} from '@/components/ui/Input';
 import { formatTime, addTimeDigit, removeTimeDigit } from "@/utils/timeUtils";
 
 interface TimeInputProps {
-    value: string,
-    id: string,
-    onChange: (id:string, value: string) => void;
+  value: string,
+  id: string,
+  onChange: (id:string, value: string) => void;
 }
 
 export const TimeInput: React.FC<TimeInputProps> = ({onChange, id, value}, ...props) => {
 
-    function onKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
-        const key = event.key;
-        const inputLength = value.length;
+  function onKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+    const key = event.key;
+
+    if (key === 'Backspace') {
+      const newTime = removeTimeDigit(value);
+      onChange(id, newTime);
+    } else if (/^\d$/.test(key)) {
+      const newTime = addTimeDigit(value, key);
+      onChange(id, newTime);
+    }
+  }
     
-        if (key === 'Backspace') {
-          const newTime = removeTimeDigit(value);
-          onChange(id, newTime);
-        } else if (/^\d$/.test(key)) {
-          const newTime = addTimeDigit(value, key);
-          onChange(id, newTime);
-        }
-        
-      }
-
-    // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //     //onChangeCallback(event.target.id, event.target.value);
-    // }
-
-    return (
-        <Input
-        type="text"
-        onKeyDown={onKeyDown}
-        value={value}
-        id={id}
-        {...props}
-        />
-    );
+  return (
+    <Input
+    type="text"
+    onKeyDown={onKeyDown}
+    value={value}
+    id={id}
+    {...props}
+    />
+  );
 }
 
 export default TimeInput;
