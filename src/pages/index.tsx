14111409e9d2c7a9a31827c12e2f3c42ef0e6511
Robert336 <rmazza33@gmail.com>
2024-02-14 19:05:19 -0500

    refactor: change inital state for timeRemaining and leaderPace. Plus some styling changes.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,15 +13,12 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
 
   const [startLap, setStartLap] = useState('');
-  const [timeRemaining, setTimeRemaining] = useState<string>('00:00');
-  const [leaderPace, setLeaderPace] = useState<string>('00:00');
+  const [timeRemaining, setTimeRemaining] = useState<string>('');
+  const [leaderPace, setLeaderPace] = useState<string>('');
   const [laps, setLaps] = useState<number[]>([]);
 
   function handleChange(id: string, value: string): void {
-
     if (id === 'timeRemainingInputID') {
-      // format time
-      // update state
       setTimeRemaining(value);
     }
     else if (id === 'leaderPaceInputID') {
@@ -61,14 +58,23 @@ export default function Home() {
     <main
       className={`bg-gray-900 flex min-h-screen flex-col items-center p-5 ${inter.className}`}
     >
-      <h1 className="text-xl">Last Lap Calculator</h1>
+      <div className='pb-5 pt-5'>
+        <h1 className="text-2xl">Last Lap Calculator</h1>
+        <p className='text-xs text-cyan-400'>by: Robert M.</p>
+      </div>
       <div className="flex flex-col gap-5">
-        <p>Current lap</p>
-        <NumberInput onChangeCallback={setStartLap} value={startLap} />
-        <p>Time to-go</p>
-        <TimeInput onChange={handleChange} id='timeRemainingInputID'  value={timeRemaining}/>
-        <p>Leader&apos;s pace</p>
-        <TimeInput onChange={handleChange} id='leaderPaceInputID' value={leaderPace}/>
+        <div>
+          <p>Current lap</p>
+          <NumberInput onChangeCallback={setStartLap} value={startLap} />
+        </div>
+        <div>
+          <p>Time to-go</p>
+          <TimeInput onChangeCallback={handleChange} id='timeRemainingInputID'  value={timeRemaining}/>
+        </div>
+        <div>
+          <p>Leader&apos;s pace</p>
+          <TimeInput onChangeCallback={handleChange} id='leaderPaceInputID' value={leaderPace}/>
+        </div>
         <button className='bg-green-800 hover:bg-green-900 rounded h-8' onClick={handleClick}>Calculate</button>
       </div>
       <div>
