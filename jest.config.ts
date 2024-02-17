b0d3a51807125fa47de453077bacbc0f0993cb73
Robert Mazza <rmazza33@gmail.com>
2024-02-17 12:33:40 -0500

    build: add missing testing dependancies

--- jest.config.ts
+++ jest.config.ts
@@ -4,8 +4,16 @@
  */
 
 import type {Config} from 'jest';
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({
+  dir: './'
+})
 
 const config: Config = {
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -126,9 +134,9 @@ const config: Config = {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: [
+    "<rootDir>/src"
+  ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
@@ -155,10 +163,10 @@ const config: Config = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
@@ -196,4 +204,4 @@ const config: Config = {
   // watchman: true,
 };
 
-export default config;
+export default createJestConfig(config);
