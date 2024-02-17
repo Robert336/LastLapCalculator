73e829c6f417a7f153d03d8f9458d6bfadf166db
Robert Mazza <rmazza33@gmail.com>
2024-02-17 15:27:46 -0500

    feat: make responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,6 +42,7 @@ export default function Home() {
     }
 
     if (timeStrToNumber(timeRemaining) / timeStrToNumber(leaderPace) > 40) {
+      console.log(timeStrToNumber(timeRemaining), timeStrToNumber(leaderPace))
       alert('ERROR: Calculation is too large, there is either more than 40 laps left or one of your time inputs is wrong.');
       return;
     }
@@ -56,31 +57,35 @@ export default function Home() {
 
   return (
     <main
-      className={`bg-gray-900 flex min-h-screen flex-col items-center p-5 ${inter.className}`}
+      className={`bg-gray-900 flex min-h-screen flex-col items-center p-5 ${inter.className}
+        sm:grid md:grid-cols-2 sm:items-start sm:gap-10`}
     >
-      <div className='pb-5 pt-5'>
-        <h1 className="text-2xl">Last Lap Calculator</h1>
-        <p className='text-xs text-cyan-400'>by: Robert M.</p>
+      <div className='sticky top-0 sm:max-w-60 sm:justify-self-end'>
+        <div className='pb-5 pt-5'>
+          <h1 className="text-2xl">Last Lap Calculator</h1>
+          <p className='text-xs text-cyan-400'>by: Robert M.</p>
+        </div>
+        <div className="flex flex-col gap-5">
+          <div>
+            <p>Current lap</p>
+            <NumberInput onChangeCallback={setStartLap} value={startLap} />
+          </div>
+          <div>
+            <p>Time to-go</p>
+            <TimeInput onChangeCallback={handleChange} id='timeRemainingInputID'  value={timeRemaining} data-testid='timeRemainingInputID'/>
+          </div>
+          <div>
+            <p>Leader&apos;s pace</p>
+            <TimeInput onChangeCallback={handleChange} id='leaderPaceInputID' value={leaderPace}  data-testid='leaderPaceInputID'/>
+          </div>
+          <button className='bg-green-800 hover:bg-green-900 rounded h-8' onClick={handleClick} data-testid='calculateButton'>Calculate</button>
+        </div>
+      
       </div>
-      <div className="flex flex-col gap-5">
-        <div>
-          <p>Current lap</p>
-          <NumberInput onChangeCallback={setStartLap} value={startLap} />
-        </div>
-        <div>
-          <p>Time to-go</p>
-          <TimeInput onChangeCallback={handleChange} id='timeRemainingInputID'  value={timeRemaining} data-testid='timeRemainingInputID'/>
-        </div>
-        <div>
-          <p>Leader&apos;s pace</p>
-          <TimeInput onChangeCallback={handleChange} id='leaderPaceInputID' value={leaderPace}  data-testid='leaderPaceInputID'/>
-        </div>
-        <button className='bg-green-800 hover:bg-green-900 rounded h-8' onClick={handleClick} data-testid='calculateButton'>Calculate</button>
-      </div>
-      <div>
+      <div className='max-w-60'>
       {isFilled() ? 
         <LapTable startLap={startLap} laps={laps}/> : 
-        <h1 className="text-3xl text-gray-700 p-10">
+        <h1 className="text-3xl text-gray-700 p-4 text-center">
           Finish filling above to see lap table.
         </h1>
       }
